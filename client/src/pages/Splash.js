8e88d353c47d831114ab0ec1020409f66c9f9b2b
Randamus <rcviets@gmail.com>
2020-01-29 19:23:27 -0700

    splash components imported

---
+++ client/src/pages/Splash.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import Nav from "../components/navLogin";
+import Hero from "../components/splashHero";
+import Works from "../components/splashWorks";
+import Clients from "../components/splashClients";

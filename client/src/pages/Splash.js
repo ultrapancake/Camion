536918da664a26747640505293166042a6fdd944
Randamus <rcviets@gmail.com>
2020-01-29 19:25:59 -0700

    signUp components imported

--- client/src/pages/Splash.js
+++ client/src/pages/Splash.js
@@ -3,3 +3,4 @@ import Nav from "../components/navLogin";
 import Hero from "../components/splashHero";
 import Works from "../components/splashWorks";
 import Clients from "../components/splashClients";
+import Footer from '../components/footer';

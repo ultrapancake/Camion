a70301b9c5b04d4fd7b93c592fe04c76c27a51a9
Glen <ultrapancake@gmail.com>
2020-02-01 10:33:39 -0700

    component/splashHero

--- client/src/components/splashHero/index.js
+++ client/src/components/splashHero/index.js
@@ -1,2 +1,24 @@
-import React from 'react';
-import './style.css';
+import React, { Component } from "react";
+
+class Home extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const imageStyle = {
+      width: 400
+    };
+    return (
+      <div>
+        <p>It's good to be home</p>
+        <img
+          style={imageStyle}
+          src="https://i.ytimg.com/vi/N1icEHtgb3g/maxresdefault.jpg"
+        />
+      </div>
+    );
+  }
+}
+
+export default Home;

f49a43cb4187218156a6399cc7063ed279469061
Jack Larsen <jackrichardlarsen@gmail.com>
2020-02-03 19:10:53 -0700

    added content areas and photos

--- client/src/components/splashHero/index.js
+++ client/src/components/splashHero/index.js
@@ -1,2 +1,25 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
+
+const Jumbotron = () => {
+  return (
+    <div className="hero-jumbotron">
+      <div className="hero-text">
+        <h1>Streamlining Freight</h1>
+        <p className="hero-p">
+          We make it easy to find and connect with suppliers and manage drivers.
+        </p>
+      </div>
+      <div className="vehicle">
+        <img className="truck-one" src="/truck-one.svg" />
+        <img className="van-two" src="/van-two.svg" />
+        <img className="truck-two" src="/truck-two.svg" />
+        <img className="van-three" src="/van-three.svg" />
+        <img className="van-one" src="/van-one.svg" />
+        <img className="rail" src="/217608558-rail.svg" />
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;

20d8f3b8a6d61cf48bc6b296675065513703bbd6
Randamus <rcviets@gmail.com>
2020-02-06 21:41:10 -0700

    file clean up

--- client/src/pages/Splash.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import Nav from "../components/navLogin";
-import Hero from "../components/splashHero";
-import Works from "../components/splashWorks";
-import Clients from "../components/splashClients";
-import Footer from '../components/footer';

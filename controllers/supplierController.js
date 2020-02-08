10f995cd97e275ce95b8724f9928c6102555013b
Glen <ultrapancake@gmail.com>
2020-02-08 10:06:34 -0700

    supplierController

---
+++ controllers/supplierController.js
@@ -0,0 +1,69 @@
+const db = require("../models");
+
+module.exports = {
+  findAll: function(req, res) {
+    console.log("supplier findAll");
+    db.Supplier.find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    console.log("supplier findById");
+    db.Supplier
+        .findById(req.params.id)
+        .then(dbModel => {
+        res.json(dbModel);
+      })
+        .catch(err => res.status(422).json(err));
+  },
+  create: function (req, res){
+      console.log("create new supplier job, check if job exists");
+      const supplierData = {
+          jobName = req.body.jobName,
+          size = req.body.size,
+          truckType = req.body.truckType,
+          weight = req.body.weight,
+          pickUpDate = req.body.pickUpDate,
+          dropOffDate = req.body.dropOffDate,
+          pickUpLoc = req.body.pickUpLoc,
+          dropOffLoc = req.body.dropOffLoc,
+          budget = req.body.budget,
+          message = req.body.message
+      }
+
+      db.Supplier.findOne({
+          jobName: supplierData.jobName
+      }).then(Supplier =>{
+          if(Supplier){
+              res.json({
+                  error: 'This Job already exists'
+              })
+          } else {
+              db.Supplier
+              .create(req.body)
+              .then(dbModel => res.json(dbModel))
+              .catch(err => res.status(422).json(err))
+          }
+      }).catch(err => {
+        res.send('error: ' + err)
+      })
+  },
+  update: function(req, res) {
+    db.Supplier
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+
+  },
+  delete: function(req,res) {
+    console.log("delete job :" + req.params.id)
+    db.Supplier
+      .deleteOne({_id: req.params.id})
+      .then(dbModel => {
+        res.json(dbModel)
+        
+      })
+      .catch(err => res.status(422).json(err));
+  }
+};

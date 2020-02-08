b13a0835b300329ffab2469d250291956e9bb26f
Randamus <rcviets@gmail.com>
2020-02-08 12:50:54 -0700

    supply jobs update/formSubmit

--- client/src/pages/SupplyJobs.js
+++ client/src/pages/SupplyJobs.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-import { Col, Row, Container } from '../components/grid';
 import axios from 'axios'
 
 
 class SupplyJobs extends Component {
     state = {
-        results: []
-    }
-
-    handleFormSubmit = event => {
-        let jobs = {
+        jobs: {
             jobName: this.state.jobName,
             size: this.state.size,
             truckType: this.state.truckType,
@@ -21,23 +16,30 @@ class SupplyJobs extends Component {
             dropOffLoc: this.state.dropOffLoc,
             budget: this.state.budget,
             message: this.state.message
-        };
+        }
+    }
+
+    handleFormSubmit = event => {
         event.preventDefault();
-        console.log(jobs);
-            axios.post({ jobs })
-                .catch(err => console.log(err))
-                .then(function (response) {
-                    return response;
-                }).then(function (body) {
-                    console.log(body);
-                });
+        console.log(this.state.jobs);
+        axios.post('/api/supplier', this.state.jobs)
+            .catch(err => console.log(err))
+            .then(function (response) {
+                return response;
+            }).then(function (body) {
+                console.log(body);
+            });
+        this.setState({ jobs })
     };
 
 
     handleInputChange = event => {
-        const { name, value } = event.target;
+        const { jobs } = this.state;
+        const name = event.target.name;
+        const value = event.target.value;
+        jobs[name] = value;
         this.setState({
-            [name]: value
+            jobs
         });
     };
 
@@ -48,13 +50,20 @@ class SupplyJobs extends Component {
                     <row>
                         <div className="form-group">
                             <label for="jobName">Job Name</label>
-                            <input type="name" class="form-control" id="jobName" placeholder="Your Job Name Here"></input>
+                            <input
+                                onChange={this.handleInputChange}
+                                type="name" class="form-control" id="jobName" placeholder="Your Job Name Here"
+                                required value={this.state.jobs.jobName}
+                            />
                         </div>
                     </row>
                     <row>
                         <div className="form-group">
                             <label for="jobSize">Size Of Shipment</label>
-                            <select className="form-control" id="jobSize">
+                            <select 
+                                onChange={this.handleInputChange}
+                                className="form-control" id="jobSize"
+                                required value={this.state.size}>
                                 <option>TruckLoad (TL)</option>
                                 <option>Less-Than-TruckLoad (LTL)</option>
                             </select>
@@ -63,7 +72,10 @@ class SupplyJobs extends Component {
                     <row>
                         <div className="form-group">
                             <label for="truckType">Truck Type</label>
-                            <select className="form-control" id="truckType">
+                            <select 
+                                onChange={this.handleInputChange}
+                                className="form-control" id="truckType"
+                                required value={this.state.truckType}>
                                 <option>Box (Tractor Trailer)</option>
                                 <option>Refrigerated</option>
                                 <option>Hazmat</option>
@@ -75,31 +87,51 @@ class SupplyJobs extends Component {
                     <row>
                         <div className="form-group">
                             <label for="jobWeight">Shipment Weight</label>
-                            <input type="number" class="form-control" id="jobWeight" placeholder="Shipment Weight in Lbs."></input>
+                            <input 
+                                onChange={this.handleInputChange}
+                                type="number" class="form-control" id="jobWeight" placeholder="Shipment Weight in Lbs."
+                                required value={this.state.weight}
+                                />
                         </div>
                     </row>
                     <row>
                         <div className='form-group'>
                             <label for="pickUpDate">Pick Up Date</label>
-                            <input type="date" class="form-control" id="pickUpDate"></input>
+                            <input 
+                                onChange={this.handleInputChange}
+                                type="date" class="form-control" id="pickUpDate"
+                                required value={this.state.pickUpDate}
+                                />
                         </div>
                     </row>
                     <row>
                         <div className='form-group'>
                             <label for="dropOffDate">Delievery Date</label>
-                            <input type="date" class="form-control" id="dropOffDate"></input>
+                            <input 
+                                onChange={this.handleInputChange}
+                                type="date" class="form-control" id="dropOffDate"
+                                required value={this.state.dropOffDate}
+                                />
                         </div>
                     </row>
                     <row>
                         <div className="form-group">
                             <label for="pickUpLoc">Pick Up Location</label>
-                            <input type="text" class="form-control" id="pickUpLoc" placeholder="Pick Up Location"></input>
+                            <input 
+                                onChange={this.handleInputChange}
+                                type="text" class="form-control" id="pickUpLoc" placeholder="Pick Up Location"
+                                required value={this.state.pickUpLoc}
+                                />
                         </div>
                     </row>
                     <row>
                         <div className="form-group">
                             <label for="dropOffLoc">Delievery Location</label>
-                            <input type="text" class="form-control" id="dropOffLoc" placeholder="Delivery Location"></input>
+                            <input 
+                                onChange={this.handleInputChange}
+                                type="text" class="form-control" id="dropOffLoc" placeholder="Delivery Location"
+                                required value={this.state.dropOffLoc}
+                                />
                         </div>
                     </row>
                     <row>
@@ -107,7 +139,11 @@ class SupplyJobs extends Component {
                             <div class="input-group-prepend">
                                 <span class="input-group-text">$</span>
                             </div>
-                            <input type="text" class="form-control" id='budget' aria-label="Amount (to the nearest dollar)"></input>
+                            <input 
+                                onChange={this.handleInputChange}
+                                type="text" class="form-control" id='budget' aria-label="Amount (to the nearest dollar)"
+                                required value={this.state.budget}
+                                />
                             <div class="input-group-append">
                                 <span class="input-group-text">.00</span>
                             </div>
@@ -116,11 +152,14 @@ class SupplyJobs extends Component {
                     <row>
                         <div class="form-group">
                             <label for="message">Special Instructions</label>
-                            <textarea class="form-control" id="message" rows="3" placeholder='Enter Special Instructions Here'></textarea>
+                            <textarea 
+                                onChange={this.handleInputChange}
+                                class="form-control" id="message" rows="3" placeholder='Enter Special Instructions Here'
+                                />
                         </div>
                     </row>
                     <row>
-                        <input class="btn btn-primary" type="submit" value="Submit"></input>
+                        <button onClick={this.handleFormSubmit} class="btn btn-primary" type="submit" value="Submit" />
                     </row>
                 </form>
             </div>
--- client/src/pages/findJobs.js
+++ client/src/pages/findJobs.js
@@ -3,85 +3,82 @@ import { Redirect } from 'react-router-dom';
 import { Col, Row, Container } from '../components/grid';
 import { List, ListItem } from "../components/list";
 import axios from 'axios'
-import DeleteBtn from '../../../../../../Week 20_React/01-Activities/11-Stu_ReactRouter/Solved/client/src/components/DeleteBtn';
+import DeleteBtn from '../components/deleteBtn';
 
 //Broker and Carrier Dash
 
 class findJobs extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            jobs: [],
-            jobName: '',
-            size: '',
-            truckType: '',
-            weight: '',
-            pickUpDate: '',
-            dropOffDate: '',
-            pickUpLoc: '',
-            dropOffLoc: '',
-            budget: '',
-            message: '',
-        };
-    };
-
-    componentDidMount() {
-        this.loadJobs();
+    state = {
+        jobs: []
     }
-    loadJobs = () => {
-        axios.get('/api/jobs')
-            .then(res =>
-                this.setState({
-                    jobName: res.data, size: '', truckType: '',
-                    weight: '', pickUpDate: '', dropOffDate: '',
-                    pickUpLoc: '', dropOffLoc: '', budget: '', message: ''
-                })
-            )
-            .catch(err => console.log(err));
-    };
+    findJobs = () => {
+        const [availableJobs, setJobs] = useState([])
+        useEffect(() => {
+            axios.get('api/jobs')
+                .then(res => {
+                    console.log('res', res.data)
+                    setJobs(res.data)
+                }).catch(err => console.log(err))
+        }, []);
+        console.log(availableJobs);
 
-    deleteJob = id => {
-        axios.delete('/api/jobs/' + id)
-            .then(res => this.loadJobs())
-            .catch(err => console.log(err));
-    };
-
-    handleInputChange = event => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
-    };
+        deleteJob((id) => {
+            axios.delete('/api/jobs/' + id)
+                .then(res => {
+                    console.log('res', res.data)
+                    setJobs(res.data)
+                }).catch(err => console.log(err))
+        }, []);
+    }
 
     render() {
         return (
-            <Container fluid>
-                <Row>
-                    <Col size='md-6 sm-12'>
-                        {this.state.jobs.length ? (
-                            <List>
-                                {this.state.jobs.map(job => (
-                                    <ListItem key={jobs._id}>
-                                        <strong>
-                                            {job.name}
-                                        </strong>
-                                        <p>
-                                            {job.size, job.size, job.truckType, job.weight, job.pickUpDate,
-                                            job.dropOffDate, job.pickUpLoc, job.dropOffLoc,
-                                            job.budget, job.message}
-                                        </p>
-                                        <DeleteBtn onClick={() => this.deleteJob(job._id)} />
-                                    </ListItem>
-                                ))}
-                            </List>
-                        ) : (
-                                <h3>No Results</h3>
-                            )}
-                    </Col>
-                </Row>
-            </Container>
+            <div className='findJobsContainer'>
+                {availableJobs.map((jobs) => {
+                    return (
+                        <row>
+                            <div class="card">
+                                <h5 class="card-header">{jobs.Job}</h5>
+                                <div class="card-body">
+                                    <h5 class="card-title"></h5>
+                                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </row>
+                    )
+                })}
+            </div>
         )
     }
 }
 
 export default findJobs;
+
+        // <div>
+        // <Container fluid>
+        //     <Row>
+        //         <Col size='md-6 sm-12'>
+        //             {this.state.jobs.length ? (
+        //                 <List>
+        //                     {this.state.jobs.map(job => (
+        //                         <ListItem key={jobs._id}>
+        //                             <strong>
+        //                                 {job.name}
+        //                             </strong>
+        //                             <p>
+        //                                 {job.size, job.size, job.truckType, job.weight, job.pickUpDate,
+        //                                     job.dropOffDate, job.pickUpLoc, job.dropOffLoc,
+        //                                     job.budget, job.message}
+        //                             </p>
+        //                             <DeleteBtn onClick={() => this.deleteJob(job._id)} />
+        //                         </ListItem>
+        //                     ))}
+        //                 </List>
+        //             ) : (
+        //                     <h3>No Results</h3>
+        //                 )}
+        //         </Col>
+        //     </Row>
+        // </Container>
+        // </div>

d1d143d4cb9da344d9c84c074ff19f8dc6ffc2fe
Randamus <rcviets@gmail.com>
2020-02-10 18:34:35 -0700

    submit Job button now redirects to find jobs page

--- client/src/pages/SupplyJobs.js
+++ client/src/pages/SupplyJobs.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import axios from "axios";
 import Footer from "../components/footer";
+import { Link } from "react-router-dom";
 
 class SupplyJobs extends Component {
   state = {
@@ -14,7 +15,8 @@ class SupplyJobs extends Component {
     pickUpLoc: "",
     dropOffLoc: "",
     budget: "",
-    message: ""
+    message: "",
+    redirectTo: null
   };
 
   handleFormSubmit = event => {
@@ -36,10 +38,10 @@ class SupplyJobs extends Component {
     axios
       .post("/api/supplier", jobsData)
       .catch(err => console.log(err))
-      .then(function(response) {
+      .then(function (response) {
         return response;
       })
-      .then(function(body) {
+      .then(function (body) {
         console.log(body);
       });
     this.setState({
@@ -52,7 +54,8 @@ class SupplyJobs extends Component {
       pickUpLoc: "",
       dropOffLoc: "",
       budget: "",
-      message: ""
+      message: "",
+      redirectTo: '/findJobs'
     });
   };
 
@@ -66,145 +69,148 @@ class SupplyJobs extends Component {
 
   render() {
     console.log("state:", this.state.jobName);
-    return (
-      <>
-        <div class="fluid-container supply-jobs-container">
-          <div className="row">
-            <div className="col-12">
-              <div className="card supply-jobs-card mx-auto">
-                <div className="card-body">
-                  <h5 className="card-title">Request a Pick Up</h5>
-                  <form>
-                    <div className="form-group job-name">
-                      <input
-                        onChange={this.handleInputChange}
-                        type="name"
-                        name="jobName"
-                        class="form-control"
-                        id="jobName"
-                        placeHolder="Your Job Name Here"
-                      />
-                    </div>
-                    <div className="form-group size-of-shipment">
+    if (this.state.redirectTo) {
+      return <Redirect to={{ pathname: this.state.redirectTo }} />;
+    } else {
+      return (
+        <>
+          <div class="fluid-container supply-jobs-container">
+            <div className="row">
+              <div className="col-12">
+                <div className="card supply-jobs-card mx-auto">
+                  <div className="card-body">
+                    <h5 className="card-title">Request a Pick Up</h5>
+                    <form>
+                      <div className="form-group job-name">
+                        <input
+                          onChange={this.handleInputChange}
+                          type="name"
+                          name="jobName"
+                          class="form-control"
+                          id="jobName"
+                          placeHolder="Your Job Name Here"
+                        />
+                      </div>
+                      <div className="form-group size-of-shipment">
                         <lable for="jobSize"><span>Size of Shipment</span></lable>
-                      <select
-                        onChange={this.handleInputChange}
-                        name="size"
-                        className="form-control"
-                        id="jobSize"
-                      >
-                        <option>TruckLoad (TL)</option>
-                        <option>Less-Than-TruckLoad (LTL)</option>
-                      </select>
-                    </div>
-                    <div className="form-group truck-type">
+                        <select
+                          onChange={this.handleInputChange}
+                          name="size"
+                          className="form-control"
+                          id="jobSize"
+                        >
+                          <option>TruckLoad (TL)</option>
+                          <option>Less-Than-TruckLoad (LTL)</option>
+                        </select>
+                      </div>
+                      <div className="form-group truck-type">
                         <label for="truckType"><span>Truck Type Needed</span></label>
-                      <select
-                        onChange={this.handleInputChange}
-                        name="truckType"
-                        className="form-control"
-                        id="truckType"
-                      >
-                        <option>Box (Tractor Trailer)</option>
-                        <option>Refrigerated</option>
-                        <option>Hazmat</option>
-                        <option>FlatBed</option>
-                        <option>Van (Light Duty)</option>
-                      </select>
-                    </div>
-                    <div className="form-group shipment-weight">
-                      <input
-                        onChange={this.handleInputChange}
-                        name="weight"
-                        type="number"
-                        class="form-control"
-                        id="jobWeight"
-                        placeholder="Shipment Weight in Lbs."
-                      />
-                    </div>
-                    <div className="form-group pick-up-date">
+                        <select
+                          onChange={this.handleInputChange}
+                          name="truckType"
+                          className="form-control"
+                          id="truckType"
+                        >
+                          <option>Box (Tractor Trailer)</option>
+                          <option>Refrigerated</option>
+                          <option>Hazmat</option>
+                          <option>FlatBed</option>
+                          <option>Van (Light Duty)</option>
+                        </select>
+                      </div>
+                      <div className="form-group shipment-weight">
+                        <input
+                          onChange={this.handleInputChange}
+                          name="weight"
+                          type="number"
+                          class="form-control"
+                          id="jobWeight"
+                          placeholder="Shipment Weight in Lbs."
+                        />
+                      </div>
+                      <div className="form-group pick-up-date">
                         <label for="pickUpDate">Pick Up Date</label>
-                      <input
-                        onChange={this.handleInputChange}
-                        name="pickUpDate"
-                        type="date"
-                        class="form-control"
-                        id="pickUpDate"
-                      />
-                    </div>
-                    <div className="form-group drop-off-date">
+                        <input
+                          onChange={this.handleInputChange}
+                          name="pickUpDate"
+                          type="date"
+                          class="form-control"
+                          id="pickUpDate"
+                        />
+                      </div>
+                      <div className="form-group drop-off-date">
                         <label for="dropOffDate">Drop Off Date</label>
-                      <input
-                        onChange={this.handleInputChange}
-                        name="dropOffDate"
-                        placeholder="Drop Off Date"
-                        type="date"
-                        class="form-control"
-                        id="dropOffDate"
-                      />
-                    </div>
-                    <div className="form-group pick-up-location">
-                      <input
-                        onChange={this.handleInputChange}
-                        name="pickUpLoc"
-                        type="text"
-                        class="form-control"
-                        id="pickUpLoc"
-                        placeholder="Pick Up Location"
-                      />
-                    </div>
-                    <div className="form-group drop-off-location">
-                      <input
-                        onChange={this.handleInputChange}
-                        name="dropOffLoc"
-                        type="text"
-                        class="form-control"
-                        id="dropOffLoc"
-                        placeholder="Delivery Location"
-                      />
-                    </div>
-                    <div className="input-group budget">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text">$</span>
+                        <input
+                          onChange={this.handleInputChange}
+                          name="dropOffDate"
+                          placeholder="Drop Off Date"
+                          type="date"
+                          class="form-control"
+                          id="dropOffDate"
+                        />
                       </div>
-                      <input
-                        onChange={this.handleInputChange}
-                        placeholder="Budget"
-                        name="budget"
-                        type="text"
-                        class="form-control"
-                        id="budget"
-                        aria-label="Amount (to the nearest dollar)"
-                      />
-                      <div class="input-group-append">
-                        <span class="input-group-text">.00</span>
+                      <div className="form-group pick-up-location">
+                        <input
+                          onChange={this.handleInputChange}
+                          name="pickUpLoc"
+                          type="text"
+                          class="form-control"
+                          id="pickUpLoc"
+                          placeholder="Pick Up Location"
+                        />
                       </div>
-                    </div>
-                    <div className="form-group special-instructions">
-                      <textarea
-                        onChange={this.handleInputChange}
-                        name="message"
-                        class="form-control"
-                        id="message"
-                        rows="3"
-                        placeholder="Enter Special Instructions Here"
-                      />
-                    </div>
-                    <button
-                      onClick={this.handleFormSubmit}
-                      className="btn btn-primary submit-button"
-                      type="submit"
-                      value="Submit"
-                    >Submit</button>
-                  </form>
+                      <div className="form-group drop-off-location">
+                        <input
+                          onChange={this.handleInputChange}
+                          name="dropOffLoc"
+                          type="text"
+                          class="form-control"
+                          id="dropOffLoc"
+                          placeholder="Delivery Location"
+                        />
+                      </div>
+                      <div className="input-group budget">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text">$</span>
+                        </div>
+                        <input
+                          onChange={this.handleInputChange}
+                          placeholder="Budget"
+                          name="budget"
+                          type="text"
+                          class="form-control"
+                          id="budget"
+                          aria-label="Amount (to the nearest dollar)"
+                        />
+                        <div class="input-group-append">
+                          <span class="input-group-text">.00</span>
+                        </div>
+                      </div>
+                      <div className="form-group special-instructions">
+                        <textarea
+                          onChange={this.handleInputChange}
+                          name="message"
+                          class="form-control"
+                          id="message"
+                          rows="3"
+                          placeholder="Enter Special Instructions Here"
+                        />
+                      </div>
+                      <button
+                        onClick={this.handleFormSubmit}
+                        className="btn btn-primary submit-button"
+                        type="submit"
+                        value="Submit">Submit</button>
+                    </form>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <Footer />
-      </>
-    );
+          <Footer />
+        </>
+      );
+    }
   }
 }
 
